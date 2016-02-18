f48ea7ae418522335fde644b35d895ba23affcdd
Johannes Stein <johannesstein@freeze-dev.com>
2016-02-18 21:23:50 +0100

    Update example

--- example/webpack.js
+++ example/webpack.js
@@ -1,11 +1,18 @@
+var fs = require('fs');
+
 var bundlebee = require('../dist/bundlebee');
 
 bundlebee(function(entry) {
   return entry('src/index.js')
     .includes(['src/**/*.js'], 'babel');
 }).then(function(bundle) {
-  bundle('webpack')
-    .writeConfig('webpack.config.js');
+  console.log(bundle);
+  console.log(bundle('webpack'));
+
+  var config = bundle('webpack')
+    .generateConfig('webpack.config.js');
+
+  fs.writeFileSync('webpack.config.js', config);
 }).catch(function(err) {
   console.log('Something went wrong.');
   console.log(err);

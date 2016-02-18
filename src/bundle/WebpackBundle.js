1090f4445f81ab27a0b12fd28259a44bd0481c41
Johannes Stein <johannesstein@freeze-dev.com>
2016-02-18 21:35:49 +0100

    Update bundle creation

--- src/bundle/WebpackBundle.js
+++ src/bundle/WebpackBundle.js
@@ -1,28 +1,30 @@
 import globToRegExp from 'glob-to-regexp';
 
+const writeConfig = function() {
+  const loaders = this.entry.files.map(function(file) {
+    return `{
+      test: ${globToRegExp(file.files).toString()},
+      loader: "${file.transform}"
+    }`;
+  });
+
+  return `module.exports = {
+    ${(this.entry.header) ? (JSON.stringify(this.entry.header) + ', ') : ''}
+
+    entry: "${this.entry.entryFile}",
+    loaders: [${loaders.join(', \n')}]
+
+    ${(this.entry.footer) ? (',\n' + JSON.stringify(this.entry.footer)) : ''}
+  };`;
+};
+
 class WebpackBundle {
   constructor(entry) {
     this.entry = entry;
   }
 
   generateConfig() {
-    const loaders = this.entry.files.map(function(file) {
-      return `{
-        test: ${globToRegExp(file.files).toString()},
-        loader: "${file.transform}"
-      }`;
-    });
-
-    return `
-      module.exports = {
-        ${(this.entry.header) ? (JSON.stringify(this.entry.header) + ', ') : ''}
-
-        entry: "${this.entry.entryFile}",
-        loaders: [${loaders.join(', \n')}]
-
-        ${(this.entry.footer) ? (',\n' + JSON.stringify(this.entry.footer)) : ''}
-      };
-    `;
+    return writeConfig.call(this);
   }
 }
 

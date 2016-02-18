9bdee436286c91bfb4ce024f57fbc222776cb758
Johannes Stein <johannesstein@freeze-dev.com>
2016-02-18 21:32:20 +0100

    Remove console messages

--- example/webpack.js
+++ example/webpack.js
@@ -6,9 +6,6 @@ bundlebee(function(entry) {
   return entry('src/index.js')
     .includes(['src/**/*.js'], 'babel');
 }).then(function(bundle) {
-  console.log(bundle);
-  console.log(bundle('webpack'));
-
   var config = bundle('webpack')
     .generateConfig('webpack.config.js');
 

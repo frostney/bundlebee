a8295d3af814c61e5630da7a087924181e61e908
Johannes Stein <johannesstein@freeze-dev.com>
2016-02-18 21:10:34 +0100

    Add header and footer

--- src/Entry.js
+++ src/Entry.js
@@ -2,11 +2,24 @@ class Entry {
   constructor(entryFile) {
     this.entryFile = entryFile;
 
-    this.files = {};
+    this.header = '';
+    this.footer = '';
+    this.files = [];
+  }
+
+  header(header) {
+    this.header = header;
+  }
+
+  footer(footer) {
+    this.footer = footer;
   }
 
   includes(files, transform) {
-    this.files = transform;
+    this.files.push({
+      files,
+      transform,
+    });
 
     return this;
   }
